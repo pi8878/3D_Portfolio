435cc6c9da517759d64a84f233d99222176cdf47
pi8878 <simangolwalifwatila99@gmail.com>
2025-04-16 11:33:30 -0500

    Included the hero section with index.js

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
+import { words } from "../constants/index.js";
 
-const words = [
-    {text: 'Ideas', imgPath: '/images/ideas.svg'},
-    {text: 'Concepts', imgPath: '/images/concepts.svg'}
-]
 
-function Hero() {
+const Hero = () => {
   return (
     <section id="hero" className="relative overflow-hidden">
         <div className="absolute top-0 left-0 z-10">
@@ -13,15 +10,36 @@ function Hero() {
         </div>
 
         <div className="hero-layout">
-        {/* LEFT: Hero CONTENT */}
+        {/* LEFT: Hero CONTENT with an immediate return in the map section*/}
         <header className = "flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
             <div className="flex flex-col gap-7">
                 <div className="hero-text">
-                    <h1>Shaping Designs</h1>
+                    <h1>
+                        Shaping
+                        <span className="slide">
+                            <span className="wrapper">
+                                {words.map((word) => (
+                                    <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
+                                        <img
+                                            src={word.imgPath}
+                                            alt={word.text}
+                                            className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                                        />
+                                        <span>{word.text}</span>
+                                    </span>
+                                ))}
+                            </span>
+                        </span>
+
+                    </h1>
                     <h1>into Real Projects</h1>
                     <h1>that Deliver Results</h1>
                 </div>
-            </div>
+                <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+                    Hi, I’m Simangolwa, a developer with a passion for code.
+                </p>
+            </div> 
+            
         </header>
 
         {/* RIGHT: 3D MODEL */}
