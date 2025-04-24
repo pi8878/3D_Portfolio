6ac271e7ddf66796ff7fe43998aa74024f124e1a
pi8878 <simangolwalifwatila99@gmail.com>
2025-04-23 20:56:58 -0500

    added position screen for home design

--- src/components/hero_models/HeroExperience.jsx
+++ src/components/hero_models/HeroExperience.jsx
@@ -26,8 +26,14 @@ const HeroExperience = () => {
         minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
         maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
       />
+        <group
+            scale={isMobile? 0.7 :1}
+            position={[0, -3.5, 0]}
+            rotation={[0, -Math.PI/4, 0]}
+        >
 
-          <Room />
+        <Room />
+        </group>
     </Canvas>
   );
 };
