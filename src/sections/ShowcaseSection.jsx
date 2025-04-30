cf341aeb14b557806df617424c5ed507ba49fd9c
pi8878 <simangolwalifwatila99@gmail.com>
2025-04-30 00:32:51 -0500

    added right side of showcaseSection

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -18,6 +18,21 @@ const ShowcaseSection = () => {
                     </div>
                 </div>
                 {/* Right */}
+                <div className="project-list-wrapper overflow-hidden">
+                    <div className="project">
+                        <div className="image-wrapper bg-[#ffefdb]">
+                            <img src="/images/project2.png" alt="Library Management Platform"/>
+                        </div>
+                        <h2>Library Management Platform</h2>
+                    </div>
+                    <div className="project">
+                        <div className="image-wrapper bg-[#ffe7eb]">
+                            <img src="/images/project3.png" alt="YC Directory"/>
+                        </div>
+                        <h2>YC Directory - A Startup Showcase App</h2>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
