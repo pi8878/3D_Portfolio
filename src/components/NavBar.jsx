6e429ff89254e633ef0ffd8ddf07309b18d7f069
pi8878 <simangolwalifwatila99@gmail.com>
2025-05-01 14:15:40 -0500

    scroll section implementation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { navLinks } from '../constants'
 
 const NavBar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = windowscrollY > 10;
+            setScrolled(true);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+
   return (
     <header className="navbar">
         <div className="inner">
@@ -9,7 +23,16 @@ const NavBar = () => {
             </a>
 
             <nav className="desktop">
-
+                <ul>
+                    {navLinks.map(({link, name}) => (
+                        <li key={name} className="group">
+                            <a href={link}>
+                                <span>{name}</span>
+                                <span className="underline"/>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </nav>
 
             <a href="#contact" className="contact-btn group">
