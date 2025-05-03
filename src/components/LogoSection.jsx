24c2c5536545f7e53921e55dcd2bade594eab4e1
pi8878 <simangolwalifwatila99@gmail.com>
2025-05-02 23:56:05 -0500

    updated entire code logSectionjsx

--- src/components/LogoSection.jsx
+++ src/components/LogoSection.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
-import {logoIconsList} from '../constants/index.js'
+import { logoIconsList } from "../constants/index.js";
 
-const LogoIcon = ({icon}) => {
-    return (
-        <div className="flex-none flex-center marquee-item">
-            <img src="icon.imgPath" alt={icon.name} />
-        </div>
-    )
-}
-
-const LogoSection = () => {
+const LogoIcon = ({ icon }) => {
   return (
-    <div className="md:my-20 my-10 relative">
-        <div className="gradient-edge"/>
-        <div className="gradient-edge"/>
-
-        <div className="marquee h-52">
-            <div className="marquee-box md:gap-12 gap-5">
-                {logoIconsList.map((icon) => (
-                    <LogoIcon key={icon.name} icon={icon}/>
-                ))}
-            </div>
-        </div>
+    <div className="flex-none flex-center marquee-item">
+      <img src={icon.imgPath} alt={icon.name} />
     </div>
-  )
-}
+  );
+};
 
-export default LogoSection
+const LogoSection = () => (
+  <div className="md:my-20 my-10 relative">
+    <div className="gradient-edge" />
+    <div className="gradient-edge" />
+
+    <div className="marquee h-52">
+      <div className="marquee-box md:gap-12 gap-5">
+        {logoIconsList.map((icon, index) => (
+          <LogoIcon key={index} icon={icon} />
+        ))}
+
+        {logoIconsList.map((icon, index) => (
+          <LogoIcon key={index} icon={icon} />
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+export default LogoSection;
