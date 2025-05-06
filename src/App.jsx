3d7d0a148485ef2cea88698b0cab6ed23e084922
pi8878 <simangolwalifwatila99@gmail.com>
2025-05-06 01:27:25 -0500

    updated logosection as well as made featurecards file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // import React from 'react'
 
-import LogoSection from "./components/LogoSection.jsx"
+import LogoSection from "./sections/LogoSection.jsx"
 import NavBar from "./components/NavBar.jsx"
+import FeatureCards from "./sections/FeatureCards.jsx"
 import Hero from "./sections/Hero.jsx"
 import ShowcaseSection from "./sections/ShowcaseSection.jsx"
 
@@ -12,6 +13,7 @@ const App = () => {
         <Hero />
         <ShowcaseSection/>
         <LogoSection/>
+        <FeatureCards/>
     </>
   )
 }
