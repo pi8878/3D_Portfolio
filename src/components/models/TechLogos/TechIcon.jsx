b4154d38387302bd591bd78369ae4c1afca79b2a
pi8878 <simangolwalifwatila99@gmail.com>
2025-05-11 16:43:12 -0500

    done with the icon tech for now

--- src/components/models/TechLogos/TechIcon.jsx
+++ src/components/models/TechLogos/TechIcon.jsx
@@ -1,8 +1,39 @@
+import { Environment, Float, OrbitControls, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber';
 import React from 'react'
 
-const TechIcon = () => {
+import { useEffect } from "react";
+import * as THREE from "three";
+
+const TechIcon = ({model}) => {
+    const scene = useGLTF(model.modelPath);
+    
+    useEffect(() => {
+        if (model.name === "Interactive Developer") {
+          scene.scene.traverse((child) => {
+            if (child.isMesh) {
+              if (child.name === "Object_5") {
+                child.material = new THREE.MeshStandardMaterial({ color: "white" });
+              }
+            }
+          });
+        }
+    }, [scene]);
+
   return (
-    <div>TechIcon</div>
+    <Canvas>
+        <ambientLight intensity={0.3} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
+        <Environment preset="city"/>
+
+        <OrbitControls enableZoom={false}/>
+
+        <Float speed={5.5} rotattionIntensity={0.5} floatIntensity={0.9}>
+            <group scale={model.scale} rotation={model.rotation}> 
+                <primitive object={scene.scene} />
+            </group>
+        </Float>
+    </Canvas>
   )
 }
 
