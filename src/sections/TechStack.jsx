57a3ecab3266000d98f3646889c139876b8efecd
pi8878 <simangolwalifwatila99@gmail.com>
2025-05-12 14:45:30 -0500

    Added animation to the techstack

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -4,8 +4,16 @@ import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { techStackIcons } from '../constants'
 import TechIcon from '../components/models/TechLogos/TechIcon'
+import { useGSAP } from '@gsap/react'
+import {gsap} from 'gsap'
 
 const TechStack = () => {
+    useGSAP(() => {
+        gsap.fromTo('.tech-card', { y: 50, opacity: 0}, {
+            y:0,
+            opacity:1, duration:1, ease:'power2.inOut', stagger:0.2, scrolltrigger: {trigger: '#skills', start: 'top center'}
+        })
+    })
   return (
     <div id="skills" className="flex-center sectgion-padding">
         <div className="w-full h-full md:px-10 px-5">
