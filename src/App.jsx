7e772bb26a7551bab19e3270fe15219bbec339a6
pi8878 <simangolwalifwatila99@gmail.com>
2025-05-12 15:05:47 -0500

    added testimonials but not rendering that for now

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Hero from "./sections/Hero.jsx"
 import ShowcaseSection from "./sections/ShowcaseSection.jsx"
 import ExperienceSection from "./sections/ExperienceSection.jsx"
 import TechStack from "./sections/TechStack.jsx"
+import Testimonials from "./sections/Testimonials.jsx"
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <FeatureCards/>
         <ExperienceSection/>
         <TechStack/>
+        {/* <Testimonials/> */}
     </>
   )
 }
